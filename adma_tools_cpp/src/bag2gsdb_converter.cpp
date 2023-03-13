3f80b90a906166f1412804f30168e5cefb77511e
Rico Schillings <rico.schillings@hs-offenburg.de>
2023-03-13 12:58:40 +0100

    #9: also add gsdb logger for ros1
    - activated by default (disabling by launch arg when required)
    - added additional launch file for converting rosbag to gsdb
    - spliited tools by py/cpp

---
+++ adma_tools_cpp/src/bag2gsdb_converter.cpp
@@ -0,0 +1,61 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <fstream>
+#include <adma_ros_driver_msgs/AdmaDataRaw.h>
+
+class Bag2GSDBConverter
+{
+        public:
+                Bag2GSDBConverter(ros::NodeHandle* _nh);
+                // virtual ~Bag2GSDBConverter();
+        private:
+                void rawDataCallback(adma_ros_driver_msgs::AdmaDataRaw newMsg);
+
+                ros::Subscriber subRawData_;
+                std::string filePath_;
+                std::ofstream gdsbFile_;
+                unsigned long msgCounter_;
+};
+
+Bag2GSDBConverter::Bag2GSDBConverter(ros::NodeHandle* n)
+{
+        ros::param::get("/bag2gsdb/rosbag_path", filePath_);
+        if(filePath_.empty()){
+                // if no filename was defined, create a new file with timestamp as name to prevent overwriting files..
+                auto now = std::chrono::system_clock::now();
+                auto in_time_t = std::chrono::system_clock::to_time_t(now);
+                std::stringstream datetime;
+                datetime << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%H-%M-%S");
+                filePath_ = datetime.str() + ".gsdb";
+                
+        }else{
+                //otherwise create a file next to the *db3 rosbag file
+                filePath_.replace(filePath_.find(".bag"), sizeof(".bag") - 1, ".gsdb");
+                
+        }
+        ROS_INFO("writing gsdb log to: %s", filePath_.c_str());
+        gdsbFile_ = std::ofstream(filePath_);
+        subRawData_ = n->subscribe("adma/data_recorded", 10, &Bag2GSDBConverter::rawDataCallback, this);
+        msgCounter_ = 0;
+}
+
+// Bag2GSDBConverter::~Bag2GSDBConverter(){
+//         ROS_INFO("closing file, written %ld messages to %s", msgCounter_, filePath_.c_str());
+//         gdsbFile_.close();
+// }
+
+void Bag2GSDBConverter::rawDataCallback(adma_ros_driver_msgs::AdmaDataRaw newMsg)
+{
+        msgCounter_++;
+        gdsbFile_.write((const char*) &newMsg.raw_data[0], newMsg.raw_data.size());
+}
+
+int main(int argc, char **argv)
+{
+        ros::init(argc, argv, "bag2gsdb");
+        ros::NodeHandle nh;
+        Bag2GSDBConverter converter = Bag2GSDBConverter(&nh);
+        ros::spin();
+
+        return 0;
+}
--- adma_tools_py/setup.py
+++ adma_tools_py/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['adma_tools'],
+     packages=['adma_tools_py'],
      package_dir={'': 'src'}
 )
 
